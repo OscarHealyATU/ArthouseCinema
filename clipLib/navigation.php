162d6045bf74f70c4066528b0dbafeccbbbaccdc
OscarHealyATU <g00424324@atu.ie>
2025-02-12 11:31:40 +0000

    created a hardcoded profile page

--- clipLib/navigation.php
+++ clipLib/navigation.php
@@ -7,7 +7,7 @@
         <li><a href="screenings.php"><strong>Buy</strong> Tickets</a></li>
         <li><a href="merch.php">Merchandise</li>
         <?php if (isset($_SESSION['username'])) {
-            echo "<li id='profileName'><a href='account/profile.php'> Welcome <strong>" . $_SESSION["username"] . "</strong></a></li>";
+            echo "<li id='profileName'><a href='profile.php'> Welcome <strong>" . $_SESSION["username"] . "</strong></a></li>";
             echo "<li> <a href='logout.php' id='logout'><strong>Log out</strong></a></li>";
         } else {
             echo "<li> <a href='login.html' id='login'><strong>Log in</strong></a></li>";
---
+++ profile.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/namedElementStyles.css">
+    <link rel="stylesheet" href="styles/troubleshootStyle.css">
+    <link rel="stylesheet" href="styles/slideShow.css">
+    <?php require 'clipLib/connectToDB.php';
+    session_start(); ?>
+    <title>Home</title>
+</head>
+
+<body>
+    <!-- navigation -->
+    <?php include 'cliplib/navigation.php'; ?>
+    <nav class="navbar" id="screeningBar">
+
+    </nav>
+    <main>
+
+      <div class="temporary">
+        <h2>Welcome Oscar</h2>
+        <img src="account/profile.png" alt="profile picture" srcset="">
+        <input type="text" name="firstName" id="" value="Oscar">
+        <input type="text" name="LastName" id="" value="Healy">
+        <input type="text" name="email" id="" value="oscar.healy101@gmail.com">
+        <input type="text" name="phone" id="" value="083 803 8985">
+        <input type="text" name="location" id="" value="Galway">
+        
+      </div>  
+
+      
+    </main>
+    <!-- footer -->
+    <!-- <?php include 'cliplib/footer.php' ?> -->
+</body>
+
+</html>
