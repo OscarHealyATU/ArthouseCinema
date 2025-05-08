ab666a7fc44cb3b80458ac428d568fda23e525bb
OscarHealyATU <G00424324@atu.ie>
2025-05-08 22:03:43 +0100

    noticed login navigation link didnt work on signup and login pages so i corrected it

--- profile.php
+++ profile.php
@@ -38,15 +38,15 @@
         </h2>
         <p>Profile Page</p>
         <label for="firstName">First Name:</label>
-        <input type="text" name="firstName" id="" value="jimmy" disabled>
+        <input type="text" name="firstName" id="" value="<?php echo htmlspecialchars($user['first_name']);?>" disabled>
         <label for="firstName">Last Name:</label>
-        <input type="text" name="LastName" id="" value="joe" disabled>
+        <input type="text" name="LastName" id="" value="<?php echo htmlspecialchars($user['last_name']);?>" disabled>
         <label for="firstName">Email:</label>
         <input type="text" name="email" id="" value="<?php echo htmlspecialchars($user['email']);?>" disabled>
         <label for="firstName">Phone:</label>
-        <input type="text" name="phone" id="" value="081 235 1235" disabled>
+        <input type="text" name="phone" id="" value="<?php echo htmlspecialchars($user['phone']);?>" disabled>
         <label for="firstName">Location:</label>
-        <input type="text" name="location" id="" value="ireland" disabled>
+        <input type="text" name="location" id="" value="<?php echo htmlspecialchars($user['location']);?>" disabled>
         
       </div>  
     </main>
